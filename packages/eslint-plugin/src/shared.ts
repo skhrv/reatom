8392593d6caab3136acbe1e77cb28129630d41e4
Ruslan Sukharev <39522630+skhrv@users.noreply.github.com>
2024-11-07 12:40:33 +0300

    fix(eslint-plugin): typeError in reatom/unit-naming-rule (#968)

--- packages/eslint-plugin/src/shared.ts
+++ packages/eslint-plugin/src/shared.ts
@@ -3,7 +3,11 @@ import type * as estree from 'estree'
 export const reatomFactoryList = ['atom', 'action', 'reaction'] as const
 export const reatomFactoryPattern = new RegExp(`^(reatom\\w+|${reatomFactoryList.join('|')})$`)
 
-export const patternNames = (pattern: estree.Pattern): estree.Identifier[] => {
+export const patternNames = (pattern: estree.Pattern | null): estree.Identifier[] => {
+  if (!pattern) {
+    return []
+  }
+
   if (pattern.type === 'AssignmentPattern') {
     return patternNames(pattern.left)
   }
@@ -13,8 +17,11 @@ export const patternNames = (pattern: estree.Pattern): estree.Identifier[] => {
   if (pattern.type === 'ArrayPattern') {
     return pattern.elements.flatMap(patternNames)
   }
+
   if (pattern.type === 'ObjectPattern') {
-    return pattern.properties.flatMap((property) => (property.key.type === 'Identifier' ? property.key : []))
+    return pattern.properties.flatMap((property) =>
+      property.type === 'Property' && property.key.type === 'Identifier' ? property.key : [],
+    )
   }
   return []
 }
